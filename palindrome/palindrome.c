6b2ee42aba3b804bd49f7a9cc7cb5b6625a30e75
talron23 <108025960+talron23@users.noreply.github.com>
2025-03-30 23:23:55 +0300

    initial commit

---
+++ palindrome/palindrome.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+/* Function to check if a string is a palindrome (ignores spaces and tabs) */
+int palindrome(char s[ ])
+{
+    int i, j;
+
+    /* remove newline that fgets() might have added at the end */
+    for (i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '\n') {
+            s[i] = '\0'; /* Make sure the string is properly terminated */
+            break; /* Stop when we find the newline */
+        }
+    }
+
+    /* (i-1) is the last valid character, now we'll compare characters from both ends */
+    for (j = i - 1, i = 0; i < j;) {
+        /* Skip any spaces or tabs from the beginning of the string */
+        if (s[i] == ' ' || s[i] == '\t') {
+            i++; /* Move i forward */
+            continue;
+        }
+
+        /* Skip spaces or tabs from the end of the string */
+        if (s[j] == ' ' || s[j] == '\t') {
+            j--; /* Move j backward */
+            continue;
+        }
+
+        /* If characters don't match, it's not a palindrome */
+        if (s[i] != s[j]) {
+            return 0; /* Not a palindrome */
+        }
+
+        j--; /* Move j backward */
+        i++; /* Move i forward */
+    }
+
+    return 1; /* It's a palindrome if we make it here */
+}
+
+int main(void) {
+    char str[81];  /* Max input length of 80 chars, plus space for null terminator */
+    int result;
+
+    /* Ask the user for input (max 80 chars) */
+    printf("Please enter a string (max 80 chars): ");
+    fgets(str, sizeof(str), stdin); /* Get the input string */
+    printf("The given string is: %s\n", str); /* Show the string back to the user */
+
+    /* Check if the input string is a palindrome and print the result */
+    result = palindrome(str);
+    if (result == 1)
+        printf("The given string is a palindrome\n");
+    else
+        printf("The given string is not a palindrome\n");
+
+    return 0;
+}
