90e336555af56aa8b8ffaf531466b8b1a9f6c0aa
tron <tron@checkpoint.com>
2025-04-04 09:26:15 +0300

    Add palindrome and bits zero count implementations with test inputs and outputs

--- bits_zero_count/bits_zero_count.c
+++ bits_zero_count/bits_zero_count.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+unsigned int bits_zero_count(unsigned int num) {
+    unsigned int count = 0;
+    unsigned int total_bits = sizeof(num) * 8;
+    while (num) { /* will get out from while when we shifted out all 1 bits so number is 0 */
+        count += (num & 1);  /* Add 1 if most right bit is 1 */
+        num = num >> 1;           /* Shift right to process next bit */
+    }
+    return total_bits - count;
+}
+
 int main(void) {
-    printf("Hello, World!\n");
+    unsigned int num;
+    printf("Enter an integer: \n");
+    scanf("%u", &num);  /* Read an unsigned integer from input */
+    printf("You entered: %u\n", num);
+    printf("Number of zero bits are: %d\n", bits_zero_count(num)); /* print result */
     return 0;
 }
